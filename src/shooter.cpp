3dc455c0bfff9549be9d9ff616b009aa0b1920c4
MichaelGlushko <157078135+MichaelGlushko@users.noreply.github.com>
2024-05-11 23:43:47 +0000

    Question 3

--- src/shooter.cpp
+++ src/shooter.cpp
@@ -1,1 +1,22 @@
 //
+#include "shooter.h"
+#include <iostream>
+
+Roll* Shooter::throw_dice(Die& die1, Die& die2) {
+    Roll* roll = new Roll(die1, die2);
+    roll->roll_dice();
+    rolls.push_back(roll);
+    return roll;
+}
+
+void Shooter::display_rolled_values() {
+    for(Roll* roll : rolls) {
+        std::cout << roll->roll_value();
+    }
+}
+
+Shooter::~Shooter() {
+    for(Roll* roll : rolls) {
+        delete roll;
+    }
+}
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 #include "die.h"
 #include "roll.h"
+#include "shooter.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -9,7 +10,7 @@ TEST_CASE("Verify Test Configuration", "verification") {
 
 TEST_CASE("Verify die rolls returns a value from 1 to 6") {
 	Die turn;
-	
+
 	for(int i = 1; i <= 10; i++) {
 		int value = turn.roll();
 		REQUIRE((value >= 1 && value <= 6));
@@ -24,6 +25,17 @@ TEST_CASE("Verify that die rolls return a value from 2 to 12") {
 	for(int i = 1; i <= 10; i++) {
 		turn.roll_dice();
 		int value = turn.roll_value();
+		REQUIRE((value >= 2 && value <= 12));
+	}
+}
+
+TEST_CASE("Test that shooter returns a roll and verify that the roll result is between 2-12") {
+	Die die1;
+	Die die2;
+	Shooter shooter;
+	for(int i = 1; i <= 10; i++) {
+		Roll* roll = shooter.throw_dice(die1, die2);
+		int value = roll->roll_value();
 		REQUIRE((value >= 2 && value <= 12));
 	}
 }
