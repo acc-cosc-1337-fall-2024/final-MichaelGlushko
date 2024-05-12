bcec8455232adcdf87fc5a86cda4d567d675fc7f
MichaelGlushko <157078135+MichaelGlushko@users.noreply.github.com>
2024-05-12 00:45:41 +0000

    Question 4

--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -3,6 +3,8 @@
 #include "die.h"
 #include "roll.h"
 #include "shooter.h"
+#include "come_out_phase.h"
+#include "point_phase.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -37,5 +39,29 @@ TEST_CASE("Test that shooter returns a roll and verify that the roll result is b
 		Roll* roll = shooter.throw_dice(die1, die2);
 		int value = roll->roll_value();
 		REQUIRE((value >= 2 && value <= 12));
+	}
+}
+
+TEST_CASE("Test that ComeOutPhase get outcomes returns values RollOutcome::natural, RollOutcome::craps, and RollOutcome::point") {
+	Die die1;
+	Die die2;
+	ComeOutPhase cop;
+
+	for(int i = 1; i <= 10; i++) {
+		Roll roll(die1, die2);
+		RollOutcome outcome = cop.get_outcome(&roll);
+		REQUIRE((outcome == RollOutcome::natural || outcome == RollOutcome::craps || outcome == RollOutcome::point));
+	}
+}
+
+TEST_CASE("Test that PointPhase get outcomes returns values RollOutcome::point, RollOutcome::seven_out, and RollOutcome::nopoint") {
+	Die die1;
+	Die die2;
+	PointPhase pPhase(5);
+	
+	for(int i = 1; i <= 10; i++) {
+		Roll roll(die1, die2);
+		RollOutcome outcome = pPhase.get_outcome(&roll);
+		REQUIRE((outcome == RollOutcome::point || outcome == RollOutcome::seven_out || outcome == RollOutcome::nopoint));
 	}
 }
